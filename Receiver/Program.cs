768c1b69f7d8018c847015768a9efaa26aded674
Aleksey <pshenichnov@gmail.com>
2018-03-15 07:00:28 -0400

    Demo app to simulate the problem

---
+++ Receiver/Program.cs
@@ -0,0 +1,53 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", true, true);
+
+            var configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Information("Starting Receiver...");
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton(context => Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+                IRabbitMqHost host = x.Host(new Uri("rabbitmq://guest:guest@localhost:5672/test"), h => { });
+
+                for (var i = 0; i < 50; i++)
+                {
+                    x.ReceiveEndpoint(host, $"receiver_queue{i}", e =>
+                    {
+                        e.Consumer<TestHandler>();
+                    });
+                }
+
+                x.UseSerilog();
+            }));
+
+            var container = services.BuildServiceProvider();
+
+            var busControl = container.GetRequiredService<IBusControl>();
+
+            busControl.Start();
+
+            Log.Information("Receiver started...");
+        }
+    }
+}
---
+++ Receiver/TestConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    public interface TestCommand
+    {
+        Guid Id { get; }
+    }
+
+    public class TestHandler : IConsumer<TestCommand>
+    {
+        static int count = 0;
+
+        public Task Consume(ConsumeContext<TestCommand> context)
+        {
+            var redeliveryCount = context.Headers.Get<string>("MT-Redelivery-Count");
+
+            Log.Information($"MT-Redelivery-Count: {redeliveryCount}; Total: {++count}");
+
+            throw new Exception("something went wrong...");
+        }
+    }
+}
